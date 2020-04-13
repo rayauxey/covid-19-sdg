1b5860c84a687b3c02984412ed20bc40ecc2ea5f
Ray Auxey <rayauxey@gmail.com>
2020-04-13 18:07:19 +0300

    Challege Three Attempted

--- src/estimator.js
+++ src/estimator.js
@@ -108,20 +108,20 @@ const covid19ImpactEstimator = (data) => {
   return output;
 };
 
-console.log(
-  covid19ImpactEstimator({
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 4,
-      avgDailyIncomePopulation: 0.73
-    },
-    periodType: 'days',
-    timeToElapse: 38,
-    reportedCases: 2747,
-    population: 92931687,
-    totalHospitalBeds: 678874
-  })
-);
+// console.log(
+//   covid19ImpactEstimator({
+//     region: {
+//       name: 'Africa',
+//       avgAge: 19.7,
+//       avgDailyIncomeInUSD: 4,
+//       avgDailyIncomePopulation: 0.73
+//     },
+//     periodType: 'days',
+//     timeToElapse: 38,
+//     reportedCases: 2747,
+//     population: 92931687,
+//     totalHospitalBeds: 678874
+//   })
+// );
 
-// export default covid19ImpactEstimator;
+export default covid19ImpactEstimator;
