0da08ab22e80f1595ea36c59856a6ec07487baae
Ray Auxey <rayauxey@gmail.com>
2020-04-13 21:42:28 +0300

    Backend URI

--- src/estimator.js
+++ src/estimator.js
@@ -108,20 +108,4 @@ const covid19ImpactEstimator = (data) => {
   return output;
 };
 
-// console.log(
-//   covid19ImpactEstimator({
-//     region: {
-//       name: 'Africa',
-//       avgAge: 19.7,
-//       avgDailyIncomeInUSD: 4,
-//       avgDailyIncomePopulation: 0.73
-//     },
-//     periodType: 'days',
-//     timeToElapse: 38,
-//     reportedCases: 2747,
-//     population: 92931687,
-//     totalHospitalBeds: 678874
-//   })
-// );
-
 export default covid19ImpactEstimator;
