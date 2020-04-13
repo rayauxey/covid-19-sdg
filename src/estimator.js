a351b72b35e0bec9a049c6f2e40311070b29741a
Ray Auxey <rayauxey@gmail.com>
2020-04-13 16:15:18 +0300

    Challege One Attempted

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,65 @@
-const covid19ImpactEstimator = (data) => data;
+function timeNormalizer(periodType, time) {
+  let timeMultiplier;
+
+  switch (periodType) {
+    case 'days':
+      timeMultiplier = 1;
+      break;
+    case 'weeks':
+      timeMultiplier = 7;
+      break;
+    case 'months':
+      timeMultiplier = 30;
+      break;
+    default:
+      timeMultiplier = 1;
+  }
+
+  return time * timeMultiplier;
+}
+
+function estimate(infected, days, doublerate) {
+  return infected * 2 ** Math.floor(days / doublerate);
+}
+
+const covid19ImpactEstimator = (data) => {
+  const { reportedCases, timeToElapse, periodType } = data;
+
+  const daysToDouble = 3;
+
+  const days = timeNormalizer(periodType, timeToElapse);
+
+  const currentlyInfected = reportedCases * 10;
+
+  const severeInfected = reportedCases * 50;
+
+  return {
+    data,
+    impact: {
+      currentlyInfected,
+      infectionsByRequestedTime: estimate(currentlyInfected, days, daysToDouble)
+    },
+    severeImpact: {
+      currentlyInfected: severeInfected,
+      infectionsByRequestedTime: estimate(severeInfected, days, daysToDouble)
+    }
+  };
+};
+
+// console.log(
+//   covid19ImpactEstimator({
+//     region: {
+//       name: 'Africa',
+//       avgAge: 19.7,
+//       avgDailyIncomeInUSD: 5,
+//       avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: 'days',
+//     timeToElapse: 38,
+//     reportedCases: 2747,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+//   })
+// );
 
 export default covid19ImpactEstimator;
